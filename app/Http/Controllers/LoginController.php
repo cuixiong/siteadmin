27347bfe0cc16368e06fdf59cfbf345084982186
小宝 <1192063282@qq.com>
2023-09-28 11:04:24 +0800

    职位默认分配角色ID

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Modules\Admin\Http\Models\Position;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class LoginController extends Controller
@@ -83,6 +84,8 @@ class LoginController extends Controller
             $UserModel->name = $request->get('name');
             $UserModel->position_id = $request->get('position_id');
             $UserModel->password = Hash::make($request->get('password'));// 密码使用hash值
+            // 注册时将职位选择默认的角色ID赋值到账号的role_id中
+            $UserModel->role_id = Position::where('id',$request->get('position_id'))->value('role_id');
             if(User::where('email','=',$UserModel->email)->first()){
                 ReturnJson(false,'邮箱已存在请更换其他邮箱');
             }
