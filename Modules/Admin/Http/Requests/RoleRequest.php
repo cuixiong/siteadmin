dcf0f19b00f625046308724e656eccd4e2d659f1
小宝 <1192063282@qq.com>
2023-10-26 13:57:12 +0800

    角色接口BUG

--- Modules/Admin/Http/Requests/RoleRequest.php
+++ Modules/Admin/Http/Requests/RoleRequest.php
@@ -12,22 +12,12 @@ class RoleRequest extends BaseRequest
         $rules = [
             'name' => 'required',
             'status' => 'required',
-            'rule_id' => 'required',
-            'site_rule_id' => 'required',
             'data_scope' => 'required',
             'code' => 'required',
-            'is_super_administrator' => 'required',
+            'sort' => 'required',
+            'status' => 'required',
         ];
-        $message = [
-            'name.required' => '角色名称不能为空',
-            'status.required' => '状态不能为空',
-            'rule_id.required' => '权限不能为空',
-            'site_rule_id.required' => '站点权限不能为空',
-            'data_scope.required' => '数据权限不能为空',
-            'code.required' => '编码不能为空',
-            'is_super_administrator.required' => '管理员状态为空',
-        ];
-        return $this->validateRequest($request, $rules,$message);
+        return $this->validateRequest($request, $rules);
     }
 
     /**
@@ -40,22 +30,11 @@ class RoleRequest extends BaseRequest
             'id' => 'required',
             'name' => 'required',
             'status' => 'required',
-            'rule_id' => 'required',
-            'site_rule_id' => 'required',
             'data_scope' => 'required',
             'code' => 'required',
-            'is_super_administrator' => 'required',
+            'sort' => 'required',
+            'status' => 'required',
         ];
-        $message = [
-            'id.required' => 'ID不能为空',
-            'name.required' => '角色名称不能为空',
-            'status.required' => '状态不能为空',
-            'rule_id.required' => '权限不能为空',
-            'site_rule_id.required' => '站点权限不能为空',
-            'data_scope.required' => '数据权限不能为空',
-            'code.required' => '编码不能为空',
-            'is_super_administrator.required' => '管理员状态为空',
-        ];
-        return $this->validateRequest($request, $rules,$message);
+        return $this->validateRequest($request, $rules);
     }
 }
