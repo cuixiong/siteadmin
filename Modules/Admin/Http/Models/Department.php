8aed16bbc7590deaa8051b8d9e42ae5301bf2061
小宝 <1192063282@qq.com>
2023-11-01 17:02:24 +0800

    部门增加角色多选功能

--- Modules/Admin/Http/Models/Department.php
+++ Modules/Admin/Http/Models/Department.php
@@ -6,4 +6,30 @@ class Department extends Base
 {
     // 下面即是允许入库的字段，数组形式
     protected $fillable = ['parent_id','name','sort','status','created_by','updated_by','default_role'];
+
+    /**
+     * 角色ID修改器
+     */
+    protected function setDefaultRoleAttribute($value)
+    {
+        if(is_array($value))
+        {
+            $this->attributes['default_role'] = implode(',',$value);
+        }
+    }
+    /**
+     * 角色ID获取器
+     */
+    public function getDefaultRoleAttribute($value)
+    {
+        if(isset($this->attributes['default_role']))
+        {
+            $value = explode(',',$this->attributes['default_role']);
+            foreach ($value as &$map) {
+                $map = intval($map);
+            }
+            return $value;
+        }
+        return [];
+    }
 }
