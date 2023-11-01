4deb8cd196b5e0e952d7622607e28439ceffa36e
小宝 <1192063282@qq.com>
2023-11-01 13:57:00 +0800

    去除rule的字段

--- Modules/Admin/Http/Models/Rule.php
+++ Modules/Admin/Http/Models/Rule.php
@@ -5,22 +5,7 @@ use Modules\Admin\Http\Models\Base;
 class Rule extends Base
 {
     // 下面即是允许入库的字段，数组形式
-    protected $fillable = ['parent_id','name','path','component','redirect','perm','module','controller','action','route','icon','type','sort','category','visible','keepAlive','created_by','updated_by','category'];
-
-    /**
-     * 设置route字段拦截器和填充module、controller、action字段
-     *
-     * @param  string  $value
-     * @return void
-     */
-    public function setRouteAttribute($value)
-    {
-        if(!empty($value)){
-            $this->attributes['route'] = $value;
-            // 切割路由获得控制器和方法名
-            list($this->attributes['module'],$this->attributes['controller'],$this->attributes['action']) = explode('/',$value);
-        }
-    }
+    protected $fillable = ['parent_id','name','path','component','redirect','perm','route','icon','type','sort','category','visible','keepAlive','created_by','updated_by','category'];
 
     /**
      * 递归分类权限
