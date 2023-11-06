8f7436b9b288a03076b2216c59d3776254eda73c
小宝 <1192063282@qq.com>
2023-11-06 11:09:35 +0800

    返回全部字段

--- Modules/Admin/Http/Models/DictionaryValue.php
+++ Modules/Admin/Http/Models/DictionaryValue.php
@@ -6,7 +6,7 @@ class DictionaryValue extends Base
 {
     // 设置允许入库字段,数组形式
     protected $fillable = ['name','value','parent_id','code','status','sort','remark','english_name','updated_by','created_by'];
-    public $ListSelect = ['id','name','value','status','english_name'];
+    public $ListSelect = ['*'];
     /**
      * 处理查询列表条件数组
      * @param use Illuminate\Http\Request;
