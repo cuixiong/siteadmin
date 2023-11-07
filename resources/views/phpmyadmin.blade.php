eabe9464bebf134524454eb30362653e72bef3de
小宝 <1192063282@qq.com>
2023-11-07 16:25:38 +0800

    测试PHPMYADMIN

--- resources/views/phpmyadmin.blade.php
+++ resources/views/phpmyadmin.blade.php
@@ -6,8 +6,8 @@
     <title>PHPMYADMIN</title>
 </head>
 <body>
-    <form action="http://yadmin.qyrdata.com:888/phpmyadmin_756304a8b669512f" method="get">
-        <p>说明：这里的输入表单数据，就相当于到时从MYSQL数据库查询出来的数据库信息</p>
+    <!-- <form action="http://yadmin.qyrdata.com:888/phpmyadmin_756304a8b669512f" method="get"> -->
+        <!-- <p>说明：这里的输入表单数据，就相当于到时从MYSQL数据库查询出来的数据库信息</p>
         <p>跳转过去的PHPMYADMIN到时可以在后台显示，也可以新窗口打开</p>
         <label for="">IP</label>
         <input type="hidden" name="auth_type" value="config">
@@ -20,7 +20,22 @@
         <P><input type="text" name="port"></P>
         <label for="">Db</label>
         <P><input type="text" name="db"></P>
-        <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button> -->
+        <a href="http://yadmin.qyrdata.com:888/phpmyadmin_756304a8b669512f/?auth_type=config&host=rm-7xvkfypj6g2qnfvtbro.mysql.rds.aliyuncs.com&user=export&password=Ft3seTg4q%40k9A&port=3306&db=site_platform">
+            <button type="submit">登陆数据库1</button>
+        </a>
+        <br>
+        <p></p>
+        <p></p>
+        <a href="http://yadmin.qyrdata.com:888/phpmyadmin_756304a8b669512f/?auth_type=config&host=8.219.5.215&user=mrrs&password=4MCCZYKPYpY8MPjh&port=3306&db=mrrs">
+            <button type="submit">登陆数据库2</button>
+        </a>
+        <br>
+        <p></p>
+        <p></p>
+        <a href="http://yadmin.qyrdata.com:888/phpmyadmin_756304a8b669512f/?auth_type=config&host=8.219.5.215&user=yadmin&password=mSb5X5aYXnnRSj7M&port=3306&db=yadmin">
+            <button type="submit">登陆数据库3</button>
+        </a>
+    <!-- </form> -->
 </body>
 </html>
