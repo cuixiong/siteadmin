11771afde118b5b7a92bb88535bb5ebbbddb4ab6
小宝 <1192063282@qq.com>
2023-11-17 13:59:26 +0800

    用户导入导出-2

--- Modules/Admin/Routes/api.php
+++ Modules/Admin/Routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('admin/user/export',[Modules\Admin\Http\Controllers\UserController::class,'export'])->name('用户管理:用户导出');
+
 /** 需要登陆验证但不需要权限验证的路由 */
 Route::middleware([
     'api',
@@ -37,7 +39,6 @@ Route::middleware([
     Route::get('admin/user/options','UserController@options')->name('User字典数据');
     Route::post('admin/user/change-status','UserController@changeStatus')->name('用户修改状态');
     Route::post('admin/user/import',[Modules\Admin\Http\Controllers\UserController::class,'import'])->name('用户管理:用户导入');
-    Route::get('admin/user/export',[Modules\Admin\Http\Controllers\UserController::class,'export'])->name('用户管理:用户导出');
 
     // Rule控制器
     Route::get('admin/rule/list','RuleController@list')->name('权限列表');
