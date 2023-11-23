0020d81f2b6a75a826c5c8d7754221ac82f8566d
小宝 <1192063282@qq.com>
2023-11-23 16:20:23 +0800

    拼接站点标识

--- Modules/Admin/Http/Models/Rule.php
+++ Modules/Admin/Http/Models/Rule.php
@@ -15,10 +15,11 @@ class Rule extends Base
     public function buildTree($rules,$roleCodes, $parentId = 0,)
     {
         $tree = [];
+        $X_site = request()->header('X_site');
         foreach ($rules as $item) {
             $data = [];
             $data['path'] = $item['path'];
-            $data['component'] = $item['component'];
+            $data['component'] = $item['component'] ? ($X_site ? $X_site . '/'. $item['component'] : $item['component']) : '';
             $data['meta'] = [
                 'title' => $item['name'],
                 'hidden' => $item['visible'] == 1 ? false : true,
