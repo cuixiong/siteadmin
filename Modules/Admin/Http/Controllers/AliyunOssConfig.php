b1843a754aabdaca958d4ff99feebd9aa3fa048c
小宝 <1192063282@qq.com>
2023-12-06 10:46:18 +0800

    字典

--- Modules/Admin/Http/Controllers/AliyunOssConfig.php
+++ Modules/Admin/Http/Controllers/AliyunOssConfig.php
@@ -3,6 +3,9 @@
 namespace Modules\Admin\Http\Controllers;
 use Modules\Admin\Http\Controllers\CrudController;
 use Illuminate\Http\Request;
+use Modules\Admin\Http\Models\DictionaryValue;
+use Modules\Admin\Http\Models\Site;
+
 class AliyunOssConfig extends CrudController
 {
     public function update(Request $request)
@@ -22,5 +25,19 @@ class AliyunOssConfig extends CrudController
         } catch (\Exception $e) {
             ReturnJson(FALSE, $e->getMessage());
         }
+    }
+    public function option(Request $request)
+    {
+        $options = [];
+        $codes = ['Switch_State'];
+        $NameField = $request->HeaderLanguage == 'en' ? 'english_name as label' : 'name as label';
+        $data = DictionaryValue::whereIn('code',$codes)->where('status',1)->select('code','value',$NameField)->orderBy('sort','asc')->get()->toArray();
+        if(!empty($data)){
+            foreach ($data as $map){
+                $options[$map['code']][] = ['label' => $map['label'], 'value' => $map['value']];
+            }
+        }
+        $options['Site'] = (new Site)->GetListLabel(['id as value',$NameField],false,'',['status' => 1]);
+        ReturnJson(TRUE,'', $options);
     }
 }
