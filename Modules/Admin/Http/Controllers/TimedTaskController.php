67796ea6bdb302b53610c2db50ec0bd616373ff8
小宝 <1192063282@qq.com>
2023-12-13 16:06:50 +0800

    定时任务测试

--- Modules/Admin/Http/Controllers/TimedTaskController.php
+++ Modules/Admin/Http/Controllers/TimedTaskController.php
@@ -326,7 +326,11 @@ class TimedTaskController extends CrudController
                 }
             } else if($doAction == 'update') {
                 file_put_contents('test.txt', "\r update yes", FILE_APPEND);
+                file_put_contents('test.txt', "\r OldCommand=".$OldCommand, FILE_APPEND);
+                file_put_contents('test.txt', "\r command=".$command, FILE_APPEND);
+                file_put_contents('test.txt', "\r taskList=".$taskList, FILE_APPEND);
                 $taskList = str_replace($OldCommand, $command, $taskList);
+                file_put_contents('test.txt', "\r NewTaskList=".$taskList, FILE_APPEND);
                 $result = shell_exec('echo "'.trim($taskList,'').'" | crontab -');
                 if($result === null){
                     return true;
