bcb3824097fb8f1ad9f90e83afc82f32c137d06a
小宝 <1192063282@qq.com>
2023-12-14 17:03:05 +0800

    定时任务-11

--- Modules/Admin/Http/Controllers/TimedTaskController.php
+++ Modules/Admin/Http/Controllers/TimedTaskController.php
@@ -492,7 +492,9 @@ class TimedTaskController extends CrudController
             file_put_contents('test.txt', "\r shh CrontabList array_filter = ".json_encode($CrontabList), FILE_APPEND);
 
             $CrontabList = array_map(function($v){
-                return trim($v,'');
+                $v = trim($v,' ');
+                $v = trim($v,"\n");
+                return $v;
             },$CrontabList);
             file_put_contents('test.txt', "\r shh CrontabList array_map = ".json_encode($CrontabList), FILE_APPEND);
 
