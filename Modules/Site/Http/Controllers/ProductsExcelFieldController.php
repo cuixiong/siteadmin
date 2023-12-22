1e69855a604aad759863f1539f3ae8affe7eecaf
zqy <695326913@qq.com>
2023-12-22 11:38:08 +0800

    补个下拉接口

--- Modules/Site/Http/Controllers/ProductsExcelFieldController.php
+++ Modules/Site/Http/Controllers/ProductsExcelFieldController.php
@@ -5,6 +5,7 @@ namespace Modules\Site\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Modules\Site\Http\Controllers\CrudController;
 use Illuminate\Http\Request;
+use Modules\Admin\Http\Models\DictionaryValue;
 use Illuminate\Support\Facades\Validator;
 
 class ProductsExcelFieldController extends CrudController
@@ -62,5 +63,26 @@ class ProductsExcelFieldController extends CrudController
             }
         }
         ReturnJson(TRUE, trans('lang.request_success'));
+    }
+    
+    /**
+     * 获取搜索下拉列表
+     * @param $request 请求信息
+     */
+    public function searchDroplist(Request $request)
+    {
+        try {
+            // 状态开关
+            if ($request->HeaderLanguage == 'en') {
+                $filed = ['english_name as label', 'value'];
+            } else {
+                $filed = ['name as label', 'value'];
+            }
+            $data['status'] = (new DictionaryValue())->GetListLabel($filed, false, '', ['code' => 'Switch_State', 'status' => 1], ['sort' => 'ASC']);
+
+            ReturnJson(TRUE, trans('lang.request_success'), $data);
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
+        }
     }
 }
