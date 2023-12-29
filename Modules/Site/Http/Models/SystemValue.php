0a7ee0476b5b46a8dbfb1c5809200147fb8f436a
小宝 <1192063282@qq.com>
2023-12-29 13:44:43 +0800

    平台字段接口，文件管理器完善

---
+++ Modules/Site/Http/Models/SystemValue.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Site\Http\Models;
+
+use Modules\Site\Http\Models\Base;
+class SystemValue extends Base
+{
+    // 设置允许入库字段,数组形式
+    protected $fillable = ['parent_id','name','key','value','type','status','switch','english_name','hidden','updated_by','created_by','sort'];
+    protected $appends = ['type_text'];
+
+    public function getTypeTextAttribute()
+    {
+        if(!empty($this->attributes['type'])){
+            $text = DictionaryValue::GetNameAsCode('Platform_Type',$this->attributes['type']);
+            return $text;
+        } else {
+            return '';
+        }
+    }
+}
