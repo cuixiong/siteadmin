11a61410aa44707bf8f34cc11540d313866d854c
zqy <695326913@qq.com>
2024-01-11 09:34:36 +0800

    购物车列表默认sort报错

--- Modules/Site/Http/Controllers/ShopCartController.php
+++ Modules/Site/Http/Controllers/ShopCartController.php
@@ -13,6 +13,50 @@ use Modules\Site\Http\Models\OrderGoods;
 class ShopCartController extends CrudController
 {
     
+    /**
+     * 查询列表页
+     * @param $request 请求信息
+     * @param int $page 页码
+     * @param int $pageSize 页数
+     * @param Array $where 查询条件数组 默认空数组
+     */
+    protected function list(Request $request)
+    {
+        try {
+            $this->ValidateInstance($request);
+            $ModelInstance = $this->ModelInstance();
+            $model = $ModelInstance->query();
+            $model = $ModelInstance->HandleWhere($model, $request);
+            // 总数量
+            $total = $model->count();
+            // 查询偏移量
+            if (!empty($request->pageNum) && !empty($request->pageSize)) {
+                $model->offset(($request->pageNum - 1) * $request->pageSize);
+            }
+            // 查询条数
+            if (!empty($request->pageSize)) {
+                $model->limit($request->pageSize);
+            }
+            $model = $model->select($ModelInstance->ListSelect);
+            // 数据排序
+            $sort = (strtoupper($request->sort) == 'DESC') ? 'DESC' : 'ASC';
+            if (!empty($request->order)) {
+                $model = $model->orderBy($request->order, $sort);
+            } else {
+                $model = $model->orderBy('id', $sort);
+            }
+
+            $record = $model->get();
+
+            $data = [
+                'total' => $total,
+                'list' => $record
+            ];
+            ReturnJson(TRUE, trans('lang.request_success'), $data);
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
+        }
+    }
 
     /**
      * 获取搜索下拉列表
