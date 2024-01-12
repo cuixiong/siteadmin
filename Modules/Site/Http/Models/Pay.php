2031c535c9f2735be0b0b9ca023668ab67dd2cbc
小宝 <1192063282@qq.com>
2024-01-12 11:23:28 +0800

    支付模块BUG

--- Modules/Site/Http/Models/Pay.php
+++ Modules/Site/Http/Models/Pay.php
@@ -5,5 +5,18 @@ use Modules\Site\Http\Models\Base;
 class Pay extends Base
 {
     // 设置允许入库字段,数组形式
-    protected $fillable = ['name', 'image', 'info_logo', 'info_key','return_url','notify_url','sign','status','sort','content', 'updated_by', 'created_by'];
+    protected $fillable = ['name', 'image', 'info_login', 'info_key','return_url','notify_url','sign','status','sort','content', 'updated_by', 'created_by'];
+
+    public function setImageAttribute($value)
+    {
+        $value = is_array($value) && !empty($value) ? implode(',',$value) : "";
+        $this->attributes['image'] = $value;
+        return $value;
+    }
+
+    public function getImageAttribute($value)
+    {
+        $value = !empty($value) ? explode(',',$value) : [];
+        return $value;
+    }
 }
