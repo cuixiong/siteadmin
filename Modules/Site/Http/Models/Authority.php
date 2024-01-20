f75fb97ba5032bb8837e1da1b34c489f55329938
小宝 <1192063282@qq.com>
2024-01-20 13:41:42 +0800

    完善BUG

--- Modules/Site/Http/Models/Authority.php
+++ Modules/Site/Http/Models/Authority.php
@@ -5,7 +5,21 @@ use Modules\Site\Http\Models\Base;
 class Authority extends Base
 {
     // 设置允许入库字段,数组形式
-    protected $fillable = ['name', 'body','link','class_id','keyword','description','big_image','thumbnail','status','sort','updated_by', 'created_by'];
+    protected $fillable = [
+        'name', 
+        'body',
+        'link',
+        'class_id',
+        'keyword',
+        'description',
+        'big_image',
+        'thumbnail',
+        'status',
+        'sort',
+        'updated_by',
+        'created_by',
+        'class_name',
+    ];
     public function setBigImageAttribute($value)
     {
         $value = $value && is_array($value) ? implode(',',$value) : '';
