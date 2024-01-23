35c1b259b5c75a958dda7a4bc75d8a176032a3ea
小宝 <1192063282@qq.com>
2024-01-23 13:28:27 +0800

    完善BUG

--- Modules/Site/Http/Models/Partner.php
+++ Modules/Site/Http/Models/Partner.php
@@ -6,4 +6,16 @@ class Partner extends Base
 {
     // 设置允许入库字段,数组形式
     protected $fillable = ['name', 'logo','status','sort', 'updated_by', 'created_by'];
+
+    // logo修改器
+    public function setLogoAttribute($value){
+        $value = $value && is_array($value) ? implode(",",$value) : "";
+        $this->attributes['logo'] = $value;
+        return $value;
+    }
+    // logo获取器
+    public function getLogoAttribute($value){
+        $value = $value ? explode(",",$value) : [];
+        return $value;
+    }
 }
