e96a21b0b9800cf3affc2c2a283b03d40e4cca05
小宝 <1192063282@qq.com>
2024-01-23 17:02:23 +0800

    完善BUG

--- Modules/Site/Http/Models/Comment.php
+++ Modules/Site/Http/Models/Comment.php
@@ -6,4 +6,27 @@ class Comment extends Base
 {
     // 设置允许入库字段,数组形式
     protected $fillable = ['title', 'image','notes','status','comment_at','sort', 'updated_by', 'created_by'];
+    // Image修改器
+    public function setImageAttribute($value){
+        $value = $value && is_array($value) ? implode(",",$value) : "";
+        $this->attributes['image'] = $value;
+        return $value;
+    }
+    // Image获取器
+    public function getImageAttribute($value){
+        $value = $value ? explode(",",$value) : [];
+        return $value;
+    }
+
+    // CommentAt修改器
+    public function setCommentAtAttribute($value){
+        $value = $value ? strtotime($value) : 0;
+        $this->attributes['comment_at'] = $value;
+        return $value;
+    }
+    // CommentAt获取器
+    public function getCommentAtAttribute($value){
+        $value = $value ? date('Y-m-d H:i:s',$value) : '';
+        return $value;
+    }
 }
