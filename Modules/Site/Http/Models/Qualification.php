821829b243e4fce2b7d8290e80bd0aedabbf4a0d
小宝 <1192063282@qq.com>
2024-01-24 11:07:16 +0800

    完善BUG

--- Modules/Site/Http/Models/Qualification.php
+++ Modules/Site/Http/Models/Qualification.php
@@ -6,4 +6,28 @@ class Qualification extends Base
 {
     // 设置允许入库字段,数组形式
     protected $fillable = ['name', 'image','thumbnail','status','sort', 'updated_by', 'created_by'];
+
+    // Image修改器
+    public function setImageAttribute($value){
+        $value = $value && is_array($value) ? implode(",",$value) : "";
+        $this->attributes['image'] = $value;
+        return $value;
+    }
+    // Image获取器
+    public function getImageAttribute($value){
+        $value = $value ? explode(",",$value) : [];
+        return $value;
+    }
+
+    // Thumbnail修改器
+    public function setThumbnailAttribute($value){
+        $value = $value && is_array($value) ? implode(",",$value) : "";
+        $this->attributes['thumbnail'] = $value;
+        return $value;
+    }
+    // Thumbnail获取器
+    public function getThumbnailAttribute($value){
+        $value = $value ? explode(",",$value) : [];
+        return $value;
+    }
 }
