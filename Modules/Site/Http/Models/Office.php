4bca0ce6d91faf6c192bd51b47daa5aa079a2339
小宝 <1192063282@qq.com>
2024-01-25 11:02:59 +0800

    完善BUG

--- Modules/Site/Http/Models/Office.php
+++ Modules/Site/Http/Models/Office.php
@@ -5,5 +5,46 @@ use Modules\Site\Http\Models\Base;
 class Office extends Base
 {
     // 设置允许入库字段,数组形式
-    protected $fillable = ['city', 'name','language_alias','region','area','image','national_flag','phone','address','working_language','working_time','time_zone','status','sort', 'updated_by', 'created_by'];
+    protected $fillable = [
+        'city', 
+        'name',
+        'language_alias',
+        'region',
+        'area',
+        'image',
+        'national_flag',
+        'phone',
+        'address',
+        'working_language',
+        'working_time',
+        'time_zone',
+        'status',
+        'sort',
+        'updated_by', 
+        'created_by'
+    ];
+
+    // Image修改器
+    public function setImageAttribute($value){
+        $value = $value && is_array($value) ? implode(",",$value) : "";
+        $this->attributes['image'] = $value;
+        return $value;
+    }
+    // Image获取器
+    public function getImageAttribute($value){
+        $value = $value ? explode(",",$value) : [];
+        return $value;
+    }
+
+    // NationalFlag修改器
+    public function setNationalFlagAttribute($value){
+        $value = $value && is_array($value) ? implode(",",$value) : "";
+        $this->attributes['national_flag'] = $value;
+        return $value;
+    }
+    // NationalFlag获取器
+    public function getNationalFlagAttribute($value){
+        $value = $value ? explode(",",$value) : [];
+        return $value;
+    }
 }
