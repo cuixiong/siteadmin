24aeeda514233e1bed98dc39d842444b49e2930d
小宝 <1192063282@qq.com>
2024-02-01 14:09:40 +0800

    完善BUG

--- Modules/Site/Http/Requests/EmailRequest.php
+++ Modules/Site/Http/Requests/EmailRequest.php
@@ -13,7 +13,7 @@ class EmailRequest extends BaseRequest
             'name' => 'required',
             'email' => 'required',
             'host' => 'required',
-            'port' => 'required|between:1,65535',
+            'port' => ['required','size:1,5'],
             'encryption' => 'required',
             'password' => 'required',
         ];
@@ -22,7 +22,8 @@ class EmailRequest extends BaseRequest
             'email.required' => '邮箱不能为空',
             'host.required' => 'SMTP主机地址不能为空',
             'port.required' => 'SMTP主机端口不能为空',
-            'port.between' => 'SMTP主机端口不正确',
+            'port.required' => 'SMTP主机端口不能为空',
+            'port.size' => 'SMTP主机端口必须是在1-65535之间',
             'encryption.required' => 'SMTP加密类型不能为空',
             'password.required' => '邮箱授权码不能为空',
         ];
@@ -39,7 +40,7 @@ class EmailRequest extends BaseRequest
             'name' => 'required',
             'email' => 'required',
             'host' => 'required',
-            'port' => 'required|between:1,65535',
+            'port' => ['required','size:1,5'],
             'encryption' => 'required',
             'password' => 'required',
         ];
@@ -48,7 +49,8 @@ class EmailRequest extends BaseRequest
             'name.required' => '名称不能为空',
             'email.required' => '邮箱不能为空',
             'host.required' => 'SMTP主机地址不能为空',
-            'port.between' => 'SMTP主机端口不正确',
+            'port.required' => 'SMTP主机端口不能为空',
+            'port.size' => 'SMTP主机端口必须是在1-65535之间',
             'encryption.required' => 'SMTP加密类型不能为空',
             'password.required' => '邮箱授权码不能为空',
         ];
