f4cfb0a9d86b53eab62fc66bae914f846b2af0eb
小宝 <1192063282@qq.com>
2024-02-04 17:20:20 +0800

    迅速测试接口

--- Modules/Site/Http/Controllers/XunSearch.php
+++ Modules/Site/Http/Controllers/XunSearch.php
@@ -25,10 +25,41 @@ class XunSearch extends CrudController
         $index = $xs->index;
         $data = Products::where('id','>',0)->where('id','<',51)->limit(50)->get()->toArray();
         foreach ($data as $map){
-            $map['pid'] = $map['id'];
-            unset($map['id']);
+            $ini = [
+                "pid" => $map['id'],
+                "name" => $map['name'],
+                "english_name" => $map['english_name'],
+                "thumb" => $map['thumb'],
+                "publisher_id" => $map['publisher_id'],
+                "category_id" => $map['category_id'],
+                "country_id" => $map['country_id'],
+                "price" => $map['price'],
+                "keywords" => $map['keywords'],
+                "url" => $map['url'],
+                "published_date" => $map['published_date'],
+                "status" => $map['status'],
+                "author" => $map['author'],
+                "show_home" => $map['show_home'],
+                "have_sample" => $map['have_sample'],
+                "discount" => $map['discount'],
+                "discount_amount" => $map['discount_amount'],
+                "discount_type" => $map['discount_type'],
+                "discount_time_begin" => $map['discount_time_begin'],
+                "discount_time_end" => $map['discount_time_end'],
+                "pages" => $map['pages'],
+                "tables" => $map['tables'],
+                "hits" => $map['hits'],
+                "show_hot" => $map['show_hot'],
+                "show_recommend" => $map['show_recommend'],
+                "sort" => $map['sort'],
+                "updated_at" => $map['updated_at'],
+                "created_at" => $map['created_at'],
+                "updated_by" => $map['updated_by'],
+                "created_by" => $map['created_by'],
+                "downloads" => $map['downloads'],
+            ];
             $doc = new XSDocument();
-            $doc->setFields($map);
+            $doc->setFields($ini);
             $index->add($doc); 
         }
     }
