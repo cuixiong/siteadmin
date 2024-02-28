088d17e1d7f25a6d7c7b25979f0fbf32198b120c
小宝 <1192063282@qq.com>
2024-02-28 13:32:14 +0800

    完善路由

--- Modules/Site/Routes/api.php
+++ Modules/Site/Routes/api.php
@@ -87,7 +87,7 @@ Route::middleware([
         Route::get('batch-update-option', [Modules\Site\Http\Controllers\ProductsController::class, 'batchUpdateOption'])->name('报告管理:批量修改参数子项');
         Route::post('batch-update', [Modules\Site\Http\Controllers\ProductsController::class, 'batchUpdate'])->name('报告管理:批量修改');
         Route::post('batch-delete', [Modules\Site\Http\Controllers\ProductsController::class, 'batchDelete'])->name('报告管理:批量删除');
-        Route::post('quick-search', [Modules\Site\Http\Controllers\ProductsController::class, 'QuickSearch'])->name('报告管理:快速搜索');
+        Route::get('quick-search', [Modules\Site\Http\Controllers\ProductsController::class, 'QuickSearch'])->name('报告管理:快速搜索');
 
     });
     
