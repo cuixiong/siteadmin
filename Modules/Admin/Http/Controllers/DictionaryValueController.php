66d306f0fd9338411464de55c5a06dd19c1baea6
zqy <695326913@qq.com>
2024-04-11 17:32:50 +0800

    字典项同步

--- Modules/Admin/Http/Controllers/DictionaryValueController.php
+++ Modules/Admin/Http/Controllers/DictionaryValueController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace Modules\Admin\Http\Controllers;
+
 use Modules\Admin\Http\Controllers\CrudController;
 use Illuminate\Http\Request;
+use Modules\Admin\Http\Models\Dictionary;
 use Modules\Admin\Http\Models\DictionaryValue;
 
-class DictionaryValueController extends CrudController{
+class DictionaryValueController extends CrudController
+{
     /**
      * 查询列表页
      * @param $request 请求信息
@@ -13,23 +16,137 @@ class DictionaryValueController extends CrudController{
      * @param int $pageSize 页数
      * @param Array $where 查询条件数组 默认空数组
      */
-    protected function get (Request $request) {
+    protected function get(Request $request)
+    {
         try {
             $search = $request->input('search');
-            $search = $search ? json_decode($search,true) : [];
+            $search = $search ? json_decode($search, true) : [];
             $search['status'] = 1;
-            if($request->code){
+            if ($request->code) {
                 $search['code'] = $request->code;
             }
-            if($request->HeaderLanguage == 'en'){
-                $filed = ['english_name as label','value'];
+            if ($request->HeaderLanguage == 'en') {
+                $filed = ['english_name as label', 'value'];
             } else {
-                $filed = ['name as label','value'];
+                $filed = ['name as label', 'value'];
             }
-            $list = (new DictionaryValue())->GetList($filed,false,'',$search);
-            ReturnJson(TRUE,trans('lang.request_success'),$list);
+            $list = (new DictionaryValue())->GetList($filed, false, '', $search);
+            ReturnJson(TRUE, trans('lang.request_success'), $list);
         } catch (\Exception $e) {
-            ReturnJson(FALSE,$e->getMessage());
+            ReturnJson(FALSE, $e->getMessage());
+        }
+    }
+
+    /**
+     * 单个新增
+     * @param $request 请求信息
+     */
+    protected function store(Request $request)
+    {
+        try {
+            $this->ValidateInstance($request);
+            $input = $request->all();
+            $record = $this->ModelInstance()->create($input);
+            if (!$record) {
+                ReturnJson(FALSE, trans('lang.add_error'));
+            }
+            Dictionary::SaveToSite(Dictionary::SAVE_TYPE_SINGLE, $request->parent_id, true);
+            ReturnJson(TRUE, trans('lang.add_success'), ['id' => $record->id]);
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
+        }
+    }
+
+    /**
+     * AJax单行删除
+     * @param $ids 主键ID
+     */
+    protected function destroy(Request $request)
+    {
+        try {
+            $this->ValidateInstance($request);
+            $ids = $request->ids;
+            if (!is_array($ids)) {
+                $ids = explode(",", $ids);
+            }
+            foreach ($ids as $id) {
+                $record = $this->ModelInstance()->find($id);
+                if ($record) {
+                    $parent_id = $record->parent_id;
+                    $record->delete();
+                    Dictionary::SaveToSite(Dictionary::SAVE_TYPE_SINGLE, $parent_id, true);
+                }
+            }
+            ReturnJson(TRUE, trans('lang.delete_success'));
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
+        }
+    }
+
+    /**
+     * AJax单个更新
+     * @param $request 请求信息
+     */
+    protected function update(Request $request)
+    {
+        try {
+            $this->ValidateInstance($request);
+            $input = $request->all();
+            $record = $this->ModelInstance()->findOrFail($request->id);
+            if (!$record->update($input)) {
+                ReturnJson(FALSE, trans('lang.update_error'));
+            }
+            Dictionary::SaveToSite(Dictionary::SAVE_TYPE_SINGLE, $request->parent_id, true);
+            ReturnJson(TRUE, trans('lang.update_success'));
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
+        }
+    }
+
+    /**
+     * 修改状态
+     * @param $request 请求信息
+     * @param $id 主键ID
+     */
+    public function changeStatus(Request $request)
+    {
+        try {
+            if (empty($request->id)) {
+                ReturnJson(FALSE, 'id is empty');
+            }
+            $record = $this->ModelInstance()->findOrFail($request->id);
+            $record->status = $request->status;
+            if (!$record->save()) {
+                ReturnJson(FALSE, trans('lang.update_error'));
+            }
+            Dictionary::SaveToSite(Dictionary::SAVE_TYPE_SINGLE, $request->parent_id, true);
+            ReturnJson(TRUE, trans('lang.update_success'));
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
+        }
+    }
+
+
+    /**
+     * 修改排序
+     * @param $request 请求信息
+     * @param $id 主键ID
+     */
+    public function changeSort(Request $request)
+    {
+        try {
+            if (empty($request->id)) {
+                ReturnJson(FALSE, 'id is empty');
+            }
+            $record = $this->ModelInstance()->findOrFail($request->id);
+            $record->sort = $request->sort;
+            if (!$record->save()) {
+                ReturnJson(FALSE, trans('lang.update_error'));
+            }
+            Dictionary::SaveToSite(Dictionary::SAVE_TYPE_SINGLE, $request->parent_id, true);
+            ReturnJson(TRUE, trans('lang.update_success'));
+        } catch (\Exception $e) {
+            ReturnJson(FALSE, $e->getMessage());
         }
     }
 }
