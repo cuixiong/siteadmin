c782f71c1809e877887ac9fb869443bb65cc3f7a
zqy <695326913@qq.com>
2024-04-12 15:42:35 +0800

    2

--- Modules/Admin/Http/Models/Country.php
+++ Modules/Admin/Http/Models/Country.php
@@ -206,7 +206,7 @@ class Country extends Base
                     $language = $language_code;
                     break;
             };
-            return $language;
+            // return $language;
             // 获取当前租户信息,取消上一个租户的连接
             $currentTenant = tenancy()->tenant;
             if ($currentTenant) {
