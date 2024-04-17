ba8ba145d5df69fdf567df216a3ec209f4353401
黑马胖纸丶 <798396652@qq.com>
2024-04-17 10:06:31 +0800

    功能修改：测试要求，新闻类型校验修改

--- Modules/Site/Http/Requests/NewsRequest.php
+++ Modules/Site/Http/Requests/NewsRequest.php
@@ -4,50 +4,52 @@ namespace Modules\Site\Http\Requests;
 
 use Modules\Admin\Http\Requests\BaseRequest;
 
-class NewsRequest extends BaseRequest
-{
+class NewsRequest extends BaseRequest {
     /**
      * 新增数据验证
-     * @param  \Illuminate\Http\Request  $request
+     *
+     * @param \Illuminate\Http\Request $request
      */
-    public function store($request)
-    {
+    public function store($request) {
         $rules = [
-            'title' => 'required',
-            'type' => 'required',
+            'title'       => 'required',
+            //'type'        => 'required',
             'description' => 'required',
-            'url' => 'required',
-            'upload_at' => 'required',
+            'url'         => 'required',
+            'upload_at'   => 'required',
         ];
         $message = [
-            'title.required' => '标题不能为空',
-            'type.required' => '类型不能为空',
+            'title.required'       => '标题不能为空',
+            //'type.required'        => '类型不能为空',
             'description.required' => '描述不能为空',
-            'url.required' => '自定义链接不能为空',
-            'upload_at.required' => '上传时间不能为空',
+            'url.required'         => '自定义链接不能为空',
+            'upload_at.required'   => '上传时间不能为空',
         ];
+
         return $this->validateRequest($request, $rules, $message);
     }
+
     /**
      * 更新数据验证
-     * @param  \Illuminate\Http\Request  $request
+     *
+     * @param \Illuminate\Http\Request $request
      */
-    public function update($request)
-    {
+    public function update($request) {
         $rules = [
-            'title' => 'required',
-            'type' => 'required',
+            'title'       => 'required',
+            //'type'        => 'required',
             'description' => 'required',
-            'url' => 'required',
-            'upload_at' => 'required',
+            'url'         => 'required',
+            'upload_at'   => 'required',
         ];
         $message = [
-            'title.required' => '标题不能为空',
-            'type.required' => '类型不能为空',
+            'title.required'       => '标题不能为空',
+            //'type.required'        => '类型不能为空',
             'description.required' => '描述不能为空',
-            'url.required' => '自定义链接不能为空',
-            'upload_at.required' => '上传时间不能为空',
+            'url.required'         => '自定义链接不能为空',
+            'upload_at.required'   => '上传时间不能为空',
         ];
+
         return $this->validateRequest($request, $rules, $message);
     }
 }
