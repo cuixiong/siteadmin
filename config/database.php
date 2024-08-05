5caef23d2bbbf474af1e37de88952a544d25c088
黑马胖纸丶 <798396652@qq.com>
2024-08-05 15:56:20 +0800

    1.相关redis配置问题

--- config/database.php
+++ config/database.php
@@ -124,7 +124,8 @@ return [
         'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            //当前单机模式, 不需要集群模式
+            //'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
