b0edf87ae8f02e844b5ae9616bb160ef2b68172e
黑马胖纸丶 <798396652@qq.com>
2024-08-28 10:46:10 +0800

    1.每日热门,精品报告功能修改

--- Modules/Site/Http/Controllers/SiteCrontabController.php
+++ Modules/Site/Http/Controllers/SiteCrontabController.php
@@ -58,11 +58,15 @@ class SiteCrontabController extends Controller {
         //3. 同步sphinx
         $this->syncProductSphinx($rs, $productIds, $products, $site);
         //4. 获取最新的报告, 设置为精品
-        $productIds = Products::query()->where("status", 1)
-                              ->orderBy("sort", "asc")
-                              ->orderBy("published_date", "desc")
-                              ->orderBy("id", "desc")
-                              ->limit(6)->pluck("id")->toArray();
+        $id_list = [];
+        $keywords_list = [];
+        $this->getHotProductList($id_list, $keywords_list);
+        $productIds = $id_list;
+//        $productIds = Products::query()->where("status", 1)
+//                              ->orderBy("sort", "asc")
+//                              ->orderBy("published_date", "desc")
+//                              ->orderBy("id", "desc")
+//                              ->limit(6)->pluck("id")->toArray();
         $updData = [
             'show_hot' => 1,
         ];
@@ -92,12 +96,16 @@ class SiteCrontabController extends Controller {
             ];
             foreach ($pcIdList as $pcId) {
                 //5. 每个分类下, 获取最新的6条报告, 并修改为推荐状态, 并同步sphinx
-                $productIds = Products::query()->where("category_id", $pcId)
-                                      ->where("status", 1)
-                                      ->orderBy("sort", "asc")
-                                      ->orderBy("published_date", "desc")
-                                      ->orderBy("id", "desc")
-                                      ->limit(6)->pluck("id")->toArray();
+                $id_list = [];
+                $keywords_list = [];
+                $this->getRecomProductList($id_list, $keywords_list, $pcId);
+//                $productIds = Products::query()->where("category_id", $pcId)
+//                                      ->where("status", 1)
+//                                      ->orderBy("sort", "asc")
+//                                      ->orderBy("published_date", "desc")
+//                                      ->orderBy("id", "desc")
+//                                      ->limit(6)->pluck("id")->toArray();
+                $productIds = $id_list;
                 $rs = Products::query()->whereIn("id", $productIds)->update($updData);
                 $this->syncProductSphinx($rs, $productIds, $products, $site);
             }
@@ -119,4 +127,63 @@ class SiteCrontabController extends Controller {
             }
         }
     }
+
+    public function getHotProductList(&$id_list, &$keywords_list, $layer = 0, $limit = 6) {
+        if ($limit <= $layer) {
+            return true;
+        }
+        $pinfo = Products::query()->where("status", 1)
+                         ->where("author", "完成报告")
+                         ->orderBy("sort", "asc")
+                         ->orderBy("published_date", "desc")
+                         ->orderBy("id", "desc");
+        if(!empty($keywords_list )){
+            //关键词不能重复
+            $pinfo = $pinfo->whereNotIn('keywords', $keywords_list);
+        }
+        if(!empty($id_list )){
+            //不能连续ID，中间间隔20
+            $last_id = end($id_list);
+            $last_id -= 20;
+            $pinfo = $pinfo->where("id" , "<=" , $last_id);
+        }
+
+        $pinfo = $pinfo->select(["id", "keywords"])->first();
+        if (!empty($pinfo)) {
+            $id_list[] = $pinfo->id;
+            $keywords_list[] = $pinfo->keywords;
+        }
+        return $this->getHotProductList($id_list, $keywords_list, ++$layer, $limit);
+    }
+
+
+    public function getRecomProductList(&$id_list, &$keywords_list, $cate_id, $layer = 0, $limit = 6) {
+        if ($limit <= $layer) {
+            return true;
+        }
+        $pinfo = Products::query()->where("status", 1)
+                         ->where("category_id", $cate_id)
+                         ->where("author", "完成报告")
+                         ->orderBy("sort", "asc")
+                         ->orderBy("published_date", "desc")
+                         ->orderBy("id", "desc");
+        if(!empty($keywords_list )){
+            //关键词不能重复
+            $pinfo = $pinfo->whereNotIn('keywords', $keywords_list);
+        }
+        if(!empty($id_list )){
+            //不能连续ID，中间间隔20
+            $last_id = end($id_list);
+            $last_id -= 20;
+            $pinfo = $pinfo->where("id" , "<=" , $last_id);
+        }
+
+        $pinfo = $pinfo->select(["id", "keywords"])->first();
+        if (!empty($pinfo)) {
+            $id_list[] = $pinfo->id;
+            $keywords_list[] = $pinfo->keywords;
+        }
+        return $this->getRecomProductList($id_list, $keywords_list, $cate_id, ++$layer, $limit);
+    }
+
 }
