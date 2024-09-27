a696cd0ec7ce4cf0c7ef9d8c1592e0765181492b
zqy <695326913@qq.com>
2024-09-27 17:25:01 +0800

    pay_code编辑不入库

--- Modules/Site/Http/Models/Pay.php
+++ Modules/Site/Http/Models/Pay.php
@@ -5,7 +5,7 @@ use Modules\Site\Http\Models\Base;
 class Pay extends Base
 {
     // 设置允许入库字段,数组形式
-    protected $fillable = ['name', 'image', 'info_login', 'info_key','return_url','notify_url','sign','status','sort','content', 'updated_by', 'created_by'];
+    protected $fillable = ['name', 'image', 'pay_code', 'info_login', 'info_key','return_url','notify_url','sign','status','sort','content', 'updated_by', 'created_by'];
 
     public function setImageAttribute($value)
     {
