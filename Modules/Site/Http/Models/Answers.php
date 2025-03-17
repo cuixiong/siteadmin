f8d012123c5f19bfee9f67228682c5b3b5b3015f
Michael Li <798396652@qq.com>
2025-03-17 09:31:45 +0800

    feat(Answers): 添加处理查询列表条件数组功能

--- Modules/Site/Http/Models/Answers.php
+++ Modules/Site/Http/Models/Answers.php
@@ -11,6 +11,8 @@
  */
 
 namespace Modules\Site\Http\Models;
+use Modules\Admin\Http\Models\User as Admin;
+
 class Answers extends Base {
     protected $table = 'answers';
     // 设置允许入库字段,数组形式
@@ -34,4 +36,43 @@ class Answers extends Base {
     {
         return $this->belongsTo(Questions::class, 'question_id', 'id')->value('title');
     }
+
+
+    /**
+     * 处理查询列表条件数组
+     * @param $model moxel
+     * @param $search 搜索条件
+     */
+    public function HandleSearch($model,$search){
+        if(!is_array($search)){
+            $search = json_decode($search,true);
+        }
+        $search = array_filter($search,function($v){
+            if(!(empty($v) && $v != "0")){
+                return true;
+            }
+        });
+        if(!empty($search)){
+            $timeArray = ['created_at','updated_at'];
+            foreach ($search as $key => $value) {
+                if(in_array($key,['name','english_name','title' , 'content'])){
+                    $model = $model->where($key,'like','%'.trim($value).'%');
+                } else if (in_array($key,$timeArray)){
+                    if(is_array($value)){
+                        $model = $model->whereBetween($key,$value);
+                    }
+                } else if(is_array($value) && !in_array($key,$timeArray)){
+                    $model = $model->whereIn($key,$value);
+                } else if (in_array($key, ['created_by','updated_by']) && !empty($value)) {
+                    $userIds = Admin::where('nickname', 'like', '%'.$value.'%')->pluck('id');
+                    $userIds = $userIds ? $userIds : [];
+                    $model = $model->whereIn($key, $userIds);
+                } else {
+                    $model = $model->where($key,$value);
+                }
+            }
+        }
+        return $model;
+    }
+
 }
