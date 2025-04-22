ef506c6c564d81619cedf1be06fe4559e2853fae
Michael Li <798396652@qq.com>
2025-04-22 15:33:00 +0800

    feat(site): 添加站点地图功能

---
+++ Modules/Site/Http/Controllers/SiteMapConfController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Site\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Modules\Admin\Http\Models\DictionaryValue;
+
+class SiteMapConfController extends CrudController {
+    /**
+     * 获取搜索下拉列表
+     *
+     * @param $request 请求信息
+     */
+    public function searchDroplist(Request $request) {
+        try {
+            $data = [];
+            // 状态开关
+            if ($request->HeaderLanguage == 'en') {
+                $field = ['english_name as label', 'value'];
+            } else {
+                $field = ['name as label', 'value'];
+            }
+            $data['status'] = (new DictionaryValue())->GetListLabel(
+                $field, false, '', ['code' => 'Switch_State', 'status' => 1], ['sort' => 'ASC']
+            );
+            ReturnJson(true, trans('lang.request_success'), $data);
+        } catch (\Exception $e) {
+            ReturnJson(false, $e->getMessage());
+        }
+    }
+
+}
--- database/sql/site20250421.sql
+++ database/sql/site20250421.sql
@@ -11,3 +11,31 @@ CREATE TABLE `personal_setting` (
   `updated_by` mediumint(9) DEFAULT '0' COMMENT '修改者',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+ALTER TABLE `news`
+    MODIFY COLUMN `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL COMMENT '内容' AFTER `description`;
+
+
+
+CREATE TABLE `site_map_conf`
+(
+    `id`         smallint                                                      NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '名称',
+    `code`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '地图code码(唯一)',
+    `loc`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '链接',
+    `xml_name`   varchar(255) COLLATE utf8mb4_unicode_ci                       DEFAULT NULL COMMENT 'xml文件昵称',
+    `sort`       int                                                           DEFAULT '0' COMMENT '排序',
+    `status`     tinyint(1) DEFAULT '1' COMMENT '状态:1代表有效,0代表无效。',
+    `created_by` int                                                           DEFAULT '0' COMMENT '数据创建者',
+    `created_at` int                                                           DEFAULT '0' COMMENT '数据创建时',
+    `updated_by` int                                                           DEFAULT '0' COMMENT '数据修改者',
+    `updated_at` int                                                           DEFAULT '0' COMMENT '数据修改时',
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE KEY `code` (`code`) USING BTREE COMMENT '编码索引',
+    KEY          `status` (`status`) USING BTREE COMMENT '状态索引'
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+
+#测试要求,修改字段长度
+ALTER TABLE `system_values`
+    MODIFY COLUMN `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '名称' AFTER `parent_id`;
