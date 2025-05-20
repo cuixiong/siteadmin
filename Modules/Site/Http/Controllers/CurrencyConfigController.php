fe60388150f65ca16fae0a8b6a82ce8c727f280f
zqy <695326913@qq.com>
2025-05-20 13:44:34 +0800

    货币配置后台接口

--- Modules/Site/Http/Controllers/CurrencyConfigController.php
+++ Modules/Site/Http/Controllers/CurrencyConfigController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Modules\Site\Http\Controllers;
+
 use App\Const\PayConst;
 use Illuminate\Http\Request;
 use Modules\Admin\Http\Models\DictionaryValue;
@@ -10,7 +11,8 @@ use Modules\Site\Http\Models\Pay;
 class CurrencyConfigController extends CrudController
 {
 
-    public function searchDroplist(Request $request) {
+    public function searchDroplist(Request $request)
+    {
         try {
             $data = [];
             // 状态开关
@@ -19,14 +21,16 @@ class CurrencyConfigController extends CrudController
             } else {
                 $field = ['name as label', 'value'];
             }
-            $data['status'] = (new DictionaryValue())->GetListLabel($field, false, '', ['code' => 'Switch_State','status' => 1], ['sort' => 'ASC']);
+            $data['status'] = (new DictionaryValue())->GetListLabel($field, false, '', ['code' => 'Switch_State', 'status' => 1], ['sort' => 'ASC']);
 
-            $data['is_first'] = (new DictionaryValue())->GetListLabel($field, false, '', ['code' => 'V_Show','status' => 1], ['sort' => 'ASC']);
-            
-            $data['is_show'] = (new DictionaryValue())->GetListLabel($field, false, '', ['code' => 'V_Show','status' => 1], ['sort' => 'ASC']);
+            $data['is_first'] = (new DictionaryValue())->GetListLabel($field, false, '', ['code' => 'V_Show', 'status' => 1], ['sort' => 'ASC']);
 
-            $data['currency_code'] = array_combine(PayConst::$coinTypeALL, PayConst::$coinTypeALL);
-            
+            $data['is_show'] = (new DictionaryValue())->GetListLabel($field, false, '', ['code' => 'V_Show', 'status' => 1], ['sort' => 'ASC']);
+
+            $data['currency_code'] = array_map(function ($item) {
+                return  ['label' => $item, 'value' => $item];
+            }, PayConst::$coinTypeALL);
+
             ReturnJson(TRUE, trans('lang.request_success'), $data);
         } catch (\Exception $e) {
             ReturnJson(FALSE, $e->getMessage());
@@ -38,7 +42,8 @@ class CurrencyConfigController extends CrudController
      *
      * @param $request 请求信息
      */
-    protected function store(Request $request) {
+    protected function store(Request $request)
+    {
         try {
             $this->ValidateInstance($request);
             $input = $request->all();
@@ -46,12 +51,12 @@ class CurrencyConfigController extends CrudController
             if (!$record) {
                 ReturnJson(false, trans('lang.add_error'));
             }
-            
+
             //同步支付方式的汇率以及税率
             $update = [];
             $update['pay_exchange_rate'] =  $record->exchange_rate;
-            $update['pay_tax_rate']=$record->tax_rate;
-            Pay::query()->where('pay_coin_type',$record->code)->update($update);
+            $update['pay_tax_rate'] = $record->tax_rate;
+            Pay::query()->where('pay_coin_type', $record->code)->update($update);
 
             ReturnJson(true, trans('lang.add_success'), ['id' => $record->id]);
         } catch (\Exception $e) {
@@ -65,7 +70,8 @@ class CurrencyConfigController extends CrudController
      *
      * @param $request 请求信息
      */
-    protected function update(Request $request) {
+    protected function update(Request $request)
+    {
         try {
             $this->ValidateInstance($request);
             $input = $request->all();
@@ -73,49 +79,27 @@ class CurrencyConfigController extends CrudController
             if (!$record->update($input)) {
                 ReturnJson(false, trans('lang.update_error'));
             }
-            
+
             //同步支付方式的汇率以及税率
             $update = [];
             $update['pay_exchange_rate'] =  $record->exchange_rate;
-            $update['pay_tax_rate']=$record->tax_rate;
-            Pay::query()->where('pay_coin_type',$record->code)->update($update);
+            $update['pay_tax_rate'] = $record->tax_rate;
+            Pay::query()->where('pay_coin_type', $record->code)->update($update);
 
             ReturnJson(true, trans('lang.update_success'));
         } catch (\Exception $e) {
             ReturnJson(false, $e->getMessage());
         }
     }
-    
+
     /**
      * 修改主货币
      *
      * @param $request 请求信息
      * @param $id      主键ID
      */
-    public function changeFirst(Request $request) {
-        try {
-            if (empty($request->id)) {
-                ReturnJson(false, 'id is empty');
-            }
-            $record = $this->ModelInstance()->findOrFail($request->id);
-            $record->is_first = $request->is_first;
-            if (!$record->save()) {
-                ReturnJson(false, trans('lang.update_error'));
-            }
-            ReturnJson(true, trans('lang.update_success'));
-        } catch (\Exception $e) {
-            ReturnJson(false, $e->getMessage());
-        }
-    }
-    
-    
-    /**
-     * 修改是否展示
-     *
-     * @param $request 请求信息
-     * @param $id      主键ID
-     */
-    public function changeShowHome(Request $request) {
+    public function changeFirst(Request $request)
+    {
         try {
             if (empty($request->id)) {
                 ReturnJson(false, 'id is empty');
@@ -131,4 +115,27 @@ class CurrencyConfigController extends CrudController
         }
     }
 
+
+    /**
+     * 修改是否展示
+     *
+     * @param $request 请求信息
+     * @param $id      主键ID
+     */
+    public function changeShowHome(Request $request)
+    {
+        try {
+            if (empty($request->id)) {
+                ReturnJson(false, 'id is empty');
+            }
+            $record = $this->ModelInstance()->findOrFail($request->id);
+            $record->is_first = $request->is_first;
+            if (!$record->save()) {
+                ReturnJson(false, trans('lang.update_error'));
+            }
+            ReturnJson(true, trans('lang.update_success'));
+        } catch (\Exception $e) {
+            ReturnJson(false, $e->getMessage());
+        }
+    }
 }
