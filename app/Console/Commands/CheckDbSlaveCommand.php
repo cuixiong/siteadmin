684be6d85c04d5a1c7dd662659f00f9338d15cdd
Michael Li <798396652@qq.com>
2025-07-28 14:27:55 +0800

    feat(email): 添加数据库主从同步异常邮件提醒功能

--- app/Console/Commands/CheckDbSlaveCommand.php
+++ app/Console/Commands/CheckDbSlaveCommand.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Console\Commands;
+
 use App\Http\Controllers\SendEmailController;
 use App\Mail\TrendsEmail;
 use Illuminate\Console\Command;
@@ -22,20 +23,23 @@ class CheckDbSlaveCommand extends Command {
 
     public function handle() {
         $slave_info_arr = DB::select('SHOW SLAVE STATUS');
-        if(!empty($slave_info_arr[0])) {
+        if (!empty($slave_info_arr[0])) {
             $slave_info = $slave_info_arr[0];
             if ($slave_info->Slave_IO_Running != 'Yes' || $slave_info->Slave_SQL_Running != 'Yes') {
-                $scene = EmailScene::where('action', 'register')->select(
-                    ['id', 'name', 'title', 'body', 'email_sender_id', 'email_recipient', 'status', 'alternate_email_id']
+                $scene = EmailScene::where('action', 'db_sync_error')->select(
+                    ['id', 'name', 'title', 'body', 'email_sender_id', 'email_recipient', 'status',
+                     'alternate_email_id']
                 )->first();
                 $senderEmail = Email::select(['name', 'email', 'host', 'port', 'encryption', 'password'])->find(
                     $scene->email_sender_id
                 );
-                $scene->title = '主从同步异常';
-                $scene->body = '数据库主从同步异常';
-                $email = '798396652@qq.com';
-                (new SendEmailController())->handlerSendEmail($scene, $email, [], $senderEmail , true);
-
+                $data['error_message'] = $slave_info->Last_SQL_Error ?? '';
+                $data['domain'] = env('APP_DOMAIN', '');
+                // 收件人的数组
+                $emails = explode(',', $scene->email_recipient);
+                foreach ($emails as $email) {
+                    (new SendEmailController())->handlerSendEmail($scene, $email, $data, $senderEmail, true);
+                }
                 echo "数据库主从同步异常".PHP_EOL;
             } else {
                 echo "数据库主从同步正常".PHP_EOL;
