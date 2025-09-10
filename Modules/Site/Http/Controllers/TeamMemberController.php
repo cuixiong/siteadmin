06334e058a07a09ec97fa574f118e25557be0c4a
Michael Li <798396652@qq.com>
2025-09-10 17:22:58 +0800

    feat(site): 同步业务修改

--- Modules/Site/Http/Controllers/TeamMemberController.php
+++ Modules/Site/Http/Controllers/TeamMemberController.php
@@ -2,59 +2,80 @@
 
 namespace Modules\Site\Http\Controllers;
 
+use DateTimeZone;
 use Illuminate\Http\Request;
 use Modules\Admin\Http\Models\DictionaryValue;
 use Modules\Site\Http\Controllers\CrudController;
 use Modules\Site\Http\Models\ProductsCategory;
 
-class TeamMemberController extends CrudController
-{
-    public function options(Request $request){
+class TeamMemberController extends CrudController {
+    public function options(Request $request) {
         $options = [];
-        $codes = ['Switch_State','Is_Analyst'];
+        $codes = ['Switch_State', 'Is_Analyst'];
         $NameField = $request->HeaderLanguage == 'en' ? 'english_name as label' : 'name as label';
-        $data = DictionaryValue::whereIn('code',$codes)->where('status',1)->select('code','value',$NameField)->orderBy('sort','asc')->get()->toArray();
-        if(!empty($data)){
-            foreach ($data as $map){
+        $data = DictionaryValue::whereIn('code', $codes)->where('status', 1)->select('code', 'value', $NameField)
+                               ->orderBy('sort', 'asc')->get()->toArray();
+        if (!empty($data)) {
+            foreach ($data as $map) {
                 $options[$map['code']][] = ['label' => $map['label'], 'value' => intval($map['value'])];
             }
         }
-        $options['category'] = ProductsCategory::where('status',1)->select('id as value','name as label')->orderBy('sort','asc')->get()->toArray();
-        ReturnJson(TRUE,'', $options);
+        $options['category'] = ProductsCategory::where('status', 1)->select('id as value', 'name as label')->orderBy(
+            'sort', 'asc'
+        )->get()->toArray();
+        //时区下拉
+        $time_zone_list = [
+            'Asia/Shanghai',
+            'Asia/Tokyo',
+            'Asia/Dubai',
+            'Europe/London',
+            'Europe/Paris',
+            'Europe/Moscow',
+            'America/New_York',
+            'America/Los_Angeles',
+            'America/Chicago',
+        ];
+        foreach ($time_zone_list as $for_time_zone){
+            $time_zone_select_list[] = [
+                'label' => $for_time_zone,
+                'value' => $for_time_zone,
+            ];
+        }
+        $options['time_zone_list'] = $time_zone_select_list;
+            ReturnJson(true, '', $options);
     }
 
     // 修改分析师状态
-    public function ChangeAnalyst(Request $request){
+    public function ChangeAnalyst(Request $request) {
         try {
             if (empty($request->id)) {
-                ReturnJson(FALSE, 'id is empty');
+                ReturnJson(false, 'id is empty');
             }
             $record = $this->ModelInstance()->findOrFail($request->id);
             $record->is_analyst = $request->analyst;
             if (!$record->save()) {
-                ReturnJson(FALSE, trans('lang.update_error'));
+                ReturnJson(false, trans('lang.update_error'));
             }
-            ReturnJson(TRUE, trans('lang.update_success'));
+            ReturnJson(true, trans('lang.update_success'));
         } catch (\Exception $e) {
-            ReturnJson(FALSE, $e->getMessage());
+            ReturnJson(false, $e->getMessage());
         }
     }
 
-
     // 修改分析师状态
-    public function changeShowProduct(Request $request){
+    public function changeShowProduct(Request $request) {
         try {
             if (empty($request->id)) {
-                ReturnJson(FALSE, 'id is empty');
+                ReturnJson(false, 'id is empty');
             }
             $record = $this->ModelInstance()->findOrFail($request->id);
             $record->show_product = $request->show_product ?? 1;
             if (!$record->save()) {
-                ReturnJson(FALSE, trans('lang.update_error'));
+                ReturnJson(false, trans('lang.update_error'));
             }
-            ReturnJson(TRUE, trans('lang.update_success'));
+            ReturnJson(true, trans('lang.update_success'));
         } catch (\Exception $e) {
-            ReturnJson(FALSE, $e->getMessage());
+            ReturnJson(false, $e->getMessage());
         }
     }
 
@@ -68,13 +89,12 @@ class TeamMemberController extends CrudController
             $this->ValidateInstance($request);
             $input = $request->all();
             //$input['is_analyst'] = $input['is_analyst'] ?? 0;
-            if(empty($input['language'])){
+            if (empty($input['language'])) {
                 $input['language'] = '';
             }
-            if(empty($input['phone'] )){
+            if (empty($input['phone'])) {
                 $input['phone'] = '';
             }
-
             $record = $this->ModelInstance()->findOrFail($request->id);
             if (!$record->update($input)) {
                 ReturnJson(false, trans('lang.update_error'));
@@ -84,5 +104,4 @@ class TeamMemberController extends CrudController
             ReturnJson(false, $e->getMessage());
         }
     }
-
 }
